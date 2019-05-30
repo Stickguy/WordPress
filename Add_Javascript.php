85430515e5069b36d324f73d708f09621d0b76dd
frankf <grey88full@yahoo.com>
2019-05-30 11:09:43 -0400

    v1

--- Add_Javascript.php
+++ Add_Javascript.php
@@ -1,5 +1,9 @@
-/* Add Javascript to WordPress */
-/* Place in functions.php */
+/* Add Javascript functions to WordPress
+*  This is useful for small snippets (jQuery, etc)
+*  More extensive Javascript should be enqueued with
+*  wp_enqueue_style or wp_enqueue_script   */
+
+/* Place snippet in functions.php */
 
 
 /* Add Javascript to WordPress Head All Pages and Posts */
@@ -15,7 +19,7 @@ add_action('wp_head', 'wpb_hook_javascript');
 
 /* Add Javascript to WordPress Post Head */
 function wpb_hook_javascript() {
-  if (is_single ('16')) { 
+  if (is_single ('16')) {
     ?>
         <script type="text/javascript">
           // your javscript code goes here
@@ -27,7 +31,7 @@ add_action('wp_head', 'wpb_hook_javascript');
 
 /* Add Javascript to WordPress Page Head */
 function wpb_hook_javascript() {
-  if (is_page ('10')) { 
+  if (is_page ('10')) {
     ?>
         <script type="text/javascript">
           // your javscript code goes here
